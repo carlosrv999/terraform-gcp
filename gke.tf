b8af9fd222b1415603ddbecddc8f5c50ddd79338
Carlos Ramirez <carlosrv125@icloud.com>
2022-07-11 15:28:18 -0500

    changes

--- gke.tf
+++ gke.tf
@@ -1,67 +1,10 @@
-resource "google_container_cluster" "cluster-1" {
-  name = "cluster-1"
-
-  addons_config {
-
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-
-    http_load_balancing {
-      disabled = false
-    }
-
-  }
-
-  enable_autopilot = false
-
-  initial_node_count = 0
-
-  location        = "us-central1"
-  networking_mode = "VPC_NATIVE"
-
-  logging_config {
-    enable_components = [
-      "SYSTEM_COMPONENTS",
-      "WORKLOADS",
-    ]
-  }
-
-  ip_allocation_policy {
-    cluster_ipv4_cidr_block  = "10.191.0.0/17"
-    services_ipv4_cidr_block = "10.191.128.0/21"
-  }
-
-  release_channel {
-    channel = "REGULAR"
-  }
-
-  default_snat_status {
-    disabled = false
-  }
-
-}
-
-resource "google_container_node_pool" "default-pool" {
-  cluster            = google_container_cluster.cluster-1.name
-  name               = "default-pool"
+resource "google_container_cluster" "cluster-emojiapp" {
+  name               = "cluster-emojiapp"
+  network            = google_compute_network.vpc_emojiapp.name
+  subnetwork         = google_compute_subnetwork.subnet_emojiapp_1.name
   initial_node_count = 1
   location           = "us-central1"
-
-  node_locations = [
-    "us-central1-a",
-    "us-central1-c",
-    "us-central1-f",
-  ]
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  oauth_scopes = [
-    "https://www.googleapis.com/auth/cloud-platform"
-  ]
+  networking_mode    = "VPC_NATIVE"
 
   node_config {
     image_type       = "COS_CONTAINERD"
@@ -85,9 +28,35 @@ resource "google_container_node_pool" "default-pool" {
 
   }
 
-  upgrade_settings {
-    max_surge       = 1
-    max_unavailable = 0
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+
+    http_load_balancing {
+      disabled = false
+    }
+
+  }
+
+  logging_config {
+    enable_components = [
+      "SYSTEM_COMPONENTS",
+      "WORKLOADS",
+    ]
+  }
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.191.0.0/17"
+    services_ipv4_cidr_block = "10.191.128.0/21"
+  }
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  default_snat_status {
+    disabled = false
   }
 
 }
