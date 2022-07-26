99d5084ea6bf687f617060314a3019f312bce8ad
Carlos Ramirez <carlosrv125@icloud.com>
2022-07-26 18:01:19 -0500

    👨‍💻 Automatic commit by Terraform

--- update-manifests.sh
+++ update-manifests.sh
@@ -5,3 +5,7 @@ mysql_vote_ip=$2
 
 sed -i "/MYSQL_HOST:/c\  MYSQL_HOST: ${mysql_emoji_ip}" ./manifests-voteapp/envs/emoji-db-access-configmap.yaml
 sed -i "/MYSQL_HOST:/c\  MYSQL_HOST: ${mysql_vote_ip}" ./manifests-voteapp/envs/vote-db-access-configmap.yaml
+
+git add .
+git commit -m "👨‍💻 Automatic commit by Terraform"
+git push origin master
