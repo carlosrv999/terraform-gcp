ba072d986aad8d9d614f9fb70c8c78b4b24d24a6
Carlos Ramirez <carlosrv125@icloud.com>
2022-07-26 18:19:55 -0500

    fixed destroy

--- delete-application-resources.sh
+++ delete-application-resources.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-kubectl delete all --all --namespace emojivote
 kubectl delete ingress --all --namespace emojivote
-kubectl delete all --all --namespace argocd
+kubectl delete svc --all --namespace argocd
 
 echo "$(date) Finished destruction of resources"
